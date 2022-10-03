a0fe2f2cfb51ed72167854ec85b4c6c7885e2602
nareerat2530 <nareerat.srisai@gmail.com>
2022-10-03 16:04:12 +0200

    trying to map events update

---
+++ UserRegistration_Tutorial/DTO/EventUpdateDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace UserRegistration_Tutorial.DTO
+{
+    public class EventUpdateDTO
+    {
+        public string Description { get; set; } = string.Empty; 
+        public DateTime EventDate { get; set; }
+    }
+}
